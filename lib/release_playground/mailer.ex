d964b64b6af718c1877241e6add6ec246ec1e913
Allan Egidio <allan.egidio@outlook.com>
2025-12-11 14:27:34 -0300

    add project files

---
+++ lib/release_playground/mailer.ex
@@ -0,0 +1,3 @@
+defmodule ReleasePlayground.Mailer do
+  use Swoosh.Mailer, otp_app: :release_playground
+end
---
+++ test/release_playground_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule ReleasePlaygroundWeb.PageControllerTest do
+  use ReleasePlaygroundWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
