8b786284c06a87d108ea9803338f4b4a7df3d745
Allan Egidio <allan.egidio@outlook.com>
2025-12-11 19:23:50 -0300

    feat!(orders): Just break it omg (#9)

--- lib/release_playground_web/live/order_live/index.ex
+++ lib/release_playground_web/live/order_live/index.ex
@@ -14,6 +14,7 @@ defmodule ReleasePlaygroundWeb.OrderLive.Index do
             <.icon name="hero-plus" /> New Order featue
           </.button>
         </:actions>
+        KKKKK
       </.header>
 
       <.table
