eb136f8154e7e36c8079567fc45075960f1c1727
Allan Egidio <allan.egidio@outlook.com>
2025-12-11 15:46:08 -0300

    add git ops

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule ReleasePlayground.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:git_ops, "~> 2.6.1", only: [:dev]}
     ]
   end
 
