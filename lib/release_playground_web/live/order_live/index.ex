5a43b89e0e75e4809df469c9fc9fe23670e37abc
Allan Egidio <allan.egidio@outlook.com>
2025-12-11 17:15:25 -0300

    feat(orders): Improve text (#2)

--- lib/release_playground_web/live/order_live/index.ex
+++ lib/release_playground_web/live/order_live/index.ex
@@ -11,7 +11,7 @@ defmodule ReleasePlaygroundWeb.OrderLive.Index do
         Listing Orders
         <:actions>
           <.button variant="primary" navigate={~p"/orders/new"}>
-            <.icon name="hero-plus" /> New Order
+            <.icon name="hero-plus" /> New Order Improve
           </.button>
         </:actions>
       </.header>
