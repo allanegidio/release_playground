af0aa30c410d0350fee87cbefe449b7f682b2ae2
Allan Egidio <allan.egidio@outlook.com>
2026-02-12 16:36:32 -0300

    fix: it worked! new fix coming to the moooooooon

--- lib/release_playground_web/live/order_live/show.ex
+++ lib/release_playground_web/live/order_live/show.ex
@@ -9,7 +9,7 @@ defmodule ReleasePlaygroundWeb.OrderLive.Show do
     <Layouts.app flash={@flash}>
       <.header>
         Order {@order.id}
-        <:subtitle>This is a asdasds record from your help note more time</:subtitle>
+        <:subtitle>This is a asdasds record from your help note more</:subtitle>
         <:actions>
           <.button navigate={~p"/orders"}>
             <.icon name="hero-arrow-left" />
